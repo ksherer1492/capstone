1ba718484624b2bc48a4d03f0cbb3cef961f24aa
daniel <daniel.m.bassett@outlook.com>
2025-05-13 09:19:16 -0500

    rename pages dir to apps dir to avoid conflicts with st.navigation()

--- home.py
+++ home.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-pages = {
-    'Home Page': [st.Page('pages/actual_home.py', title="Home Page")],
+apps = {
+    'Home Page': [st.Page('apps/actual_home.py', title="Home Page")],
     "Text Analysis": [
-        st.Page("pages/City Category Analysis.py", title="City Category Analysis"),
-        st.Page("pages/app.py", title="Customer Sentiment"),
+        st.Page("apps/City Category Analysis.py", title="City Category Analysis"),
+        st.Page("apps/app.py", title="Customer Sentiment"),
     ],
     "Census Tract Analysis": [
-        st.Page("pages/similarity_measure.py", title="Similarity Measure"),
-        st.Page("pages/Census Tract Clustering Analysis.py", title="Census Tract Clustering Analysis"),
+        st.Page("apps/similarity_measure.py", title="Similarity Measure"),
+        st.Page("apps/Census Tract Clustering Analysis.py", title="Census Tract Clustering Analysis"),
     ],
 }
 
-pg = st.navigation(pages)
+pg = st.navigation(apps)
 pg.run()
